2eed54af94d09b0be1f9588c32556cff54feb1a9
Subtirelu Mihail <mihai.subtirelu@arinetwork.com>
2015-09-01 19:13:28 +0300

    Add osx fixes

--- setup.sh
+++ setup.sh
@@ -87,6 +87,16 @@ brew cask sublime-text
 #Install docker
 brew cask install dockertoolbox
 
+
+#OSX fixes
+
+#Don't rearange Spaces
+defaults write com.apple.dock mru-spaces -bool false
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
 #The end
 brew cleanup
 source ~/.zshrc
